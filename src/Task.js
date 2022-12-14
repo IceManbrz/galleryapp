5ffcc8eab6169278f972d43b846bcafaa157158e
root <root@IceMan00.localdomain>
2022-12-14 20:40:35 +0700

    menambahkan file pertama

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Achareeya Wicaksa Putra Pribadi" // TODO: replace this
+const studentId = "2828609" // TODO: replace this
+
+export { url, studentName, studentId }
